64c38f3d80a2cfdcf8c2c41e532f7a097bac2004
rohit <rohitbhagatkar940@gmail.com>
2025-05-15 00:17:16 +0100

    very hard B

---
+++ CP31 1200/C_Contrast_Value.cpp
@@ -0,0 +1,68 @@
+/*==================================================
+  Author    : ASSaASSin
+  Created   : 14-May-2025
+  Purpose   : Competitive Programming Template
+==================================================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define Assassin
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+const ll MOD = 1e9 + 7;
+const ll INF = LONG_LONG_MAX;
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef unordered_map<ll, ll> umll;
+typedef map<ll, ll> mll;
+
+#define pb push_back
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES" << nline
+#define no cout << "NO" << nline
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define rev(i, a, b) for(int i = (a); i >= (b); i--)
+
+#ifdef Assassin
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x) ;
+#endif
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    rep(i,0,n) cin >> a[i];
+    n = unique(a.begin(), a.end()) - a.begin(); // removes all the duplicates O(n)
+    int ans = n;
+    for(int i = 0 ; i + 2 < n; i++)
+    {
+        ans -= ((a[i] < a[i+1]) && (a[i+1] < a[i+2]));
+        ans -= ((a[i] > a[i+1]) && (a[i+1] > a[i+2]));
+    }
+    cout << ans << nline;
+}
+int main ()
+{
+    fastio();
+    int tt;
+    cin >> tt;
+    while(tt--)
+    {
+        solve();
+    }
+    return 0;
+}
