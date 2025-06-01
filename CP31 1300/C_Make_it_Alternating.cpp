70a95b75b86b8eda70aa0c887707597086e4120b
rohit <rohitbhagatkar940@gmail.com>
2025-06-02 00:13:53 +0100

    good upd

---
+++ CP31 1300/C_Make_it_Alternating.cpp
@@ -0,0 +1,82 @@
+/*==================================================
+  Author    : ASSaASSin
+  Created   : 01-June-2025
+  Purpose   : Competitive Programming Template
+==================================================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define Assassin
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+const ll MOD = 1e9 + 7;
+const ll INF = LONG_LONG_MAX;
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef unordered_map<ll, ll> umll;
+typedef map<ll, ll> mll;
+
+#define pb push_back
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES" << nline
+#define no cout << "NO" << nline
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define rev(i, a, b) for(int i = (a); i >= (b); i--)
+
+int gcd(int x, int y)  { return (y ? gcd(y, x % y) : x); }
+int lcm(int x, int y)  { return x * y / gcd(x, y); }
+
+#ifdef Assassin
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x) ;
+#endif
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int n = s.size();
+    const int modulo = 998244353;
+    int l = 0;
+    int k = s.size();
+    int factans = 1;
+    while(l < n)
+    {
+        int r = l + 1;
+        while(r < n && s[l] == s[r])
+        {
+            r++;
+        }
+        factans = (1LL * factans * (r - l)) % modulo;
+        k--;
+        l = r;
+    }
+    for(int i = 1; i <= k; i++)
+    {
+        factans = (1LL * factans * i) % modulo;
+    }
+    cout << k << ' ' << factans << nline;
+}
+int main ()
+{
+    fastio();
+    int tt;
+    cin >> tt;
+    while(tt--)
+    {
+        solve();
+    }
+    return 0;
+}
