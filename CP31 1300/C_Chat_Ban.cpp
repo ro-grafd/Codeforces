69b0e3feebc572d6ce0934ded55f20e43eb39cc8
rohit <rohitbhagatkar940@gmail.com>
2025-06-05 22:58:15 +0100

    good bit manipulation

---
+++ CP31 1300/C_Chat_Ban.cpp
@@ -0,0 +1,89 @@
+/*==================================================
+  Author    : ASSaASSin
+  Created   : 05-May-2025
+  Purpose   : Competitive Programming Template
+==================================================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define Assassin
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+const ll MOD = 1e9 + 7;
+const ll INF = LONG_LONG_MAX;
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef unordered_map<ll, ll> umll;
+typedef map<ll, ll> mll;
+
+#define pb push_back
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES" << nline
+#define no cout << "NO" << nline
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define rev(i, a, b) for(int i = (a); i >= (b); i--)
+
+int gcd(int x, int y)  { return (y ? gcd(y, x % y) : x); }
+int lcm(int x, int y)  { return x * y / gcd(x, y); }
+
+#ifdef Assassin
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x) ;
+#endif
+ll count(ll x)
+{
+    return x * 1LL * (x + 1) / 2;
+}
+
+void solve()
+{
+    ll k, x;
+    cin >> k >> x;
+    ll low = 1;
+    ll high = 2* k - 1;
+    ll ans = 2 * k - 1;
+    bool over = false;
+    while(low <= high)
+    {
+        ll mid = (low + high) / 2;
+        if(mid >= k)
+        {
+            over = (count(k) + count(k - 1) - count(2*k -1 - mid) >= x);
+        }else
+        {
+            over = (count(mid) >= x);
+        }
+        if(over)
+        {
+            ans = mid;
+            high = mid - 1;
+        }else
+        {
+            low = mid + 1;
+        }
+    }
+    cout << ans << nline;
+}
+int main ()
+{
+    fastio();
+    int tt;
+    cin >> tt;
+    while(tt--)
+    {
+        solve();
+    }
+    return 0;
+}
