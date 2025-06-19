3b09393564907e110547e194695a7285a781fa9a
Rohit <rohitbhagatkar940@gmail.com>
2025-06-19 17:50:28 +0100

    Update

---
+++ Contests/ECF 179 div2 (unrated)/B_Fibonacci_Cubes.cpp
@@ -0,0 +1,142 @@
+/*==================================================
+  Author    : ASSaASSin
+  Created   : 19-June-2025
+  Purpose   : Competitive Programming Template
+==================================================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define Assassin
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+const ll MOD = 1e9 + 7;
+const ll INF = LONG_LONG_MAX;
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef unordered_map<ll, ll> umll;
+typedef map<ll, ll> mll;
+
+#define pb push_back
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES" << nline
+#define no cout << "NO" << nline
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define rev(i, a, b) for(int i = (a); i >= (b); i--)
+
+#ifdef Assassin
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x) ;
+#endif
+
+ll gcd(ll x, ll y)
+{
+    if(y == 0)
+        return x;
+    return gcd(y, x % y);
+}
+ll lcm(int x, int y) { return x * y / gcd(x, y); }
+bool isPrime(ll n)
+{
+    if(n <= 1)
+        return false;
+    if(n <= 3)
+        return true;
+    if(n % 2 == 0 || n % 3 == 0)
+        return false;
+    for(int i = 5; i * i <= n; i += 6)
+    {
+        if(n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+bool isPerfectSquare(ll x)
+{
+    if(x >= 0)
+    {
+        ll sr = sqrt(x);
+        return (sr * sr == x);
+    }
+    return false;
+}
+vll primes;
+void sieve(ll MAX_N)
+{
+    vector<bool> prime(MAX_N + 1, true);
+    prime[0] = prime[1] = false;
+    for(ll p = 2; p * p <= MAX_N; p++)
+    {
+        if(prime[p])
+        {
+            for(ll i = p * p; i <= MAX_N; i += p)
+                prime[i] = false;
+        }
+    }
+    for(ll i = 2; i <= MAX_N; i++)
+    {
+        if(prime[i])
+            primes.pb(i);
+    }
+}
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> a(m);
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            int x;
+            cin >> x;
+            a[i].pb(x);
+        }
+        sort(a[i].begin(), a[i].end());
+    }
+    vi fib(n + 1);
+    fib[0] = 1, fib[1] = 2;
+    for(int i = 2; i <= n; i++)
+    {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    for(int i = 0; i < m; i++)
+    {
+        //   one side we will need to have atleast current + previoous = f[n]
+        // and other two should greater than the max side of cube coz all others will fit in it 
+        if(a[i][0] >= fib[n-1] && a[i][1] >= fib[n-1] && a[i][2] >= fib[n])
+        {
+            cout << "1";
+        }else
+        {
+            cout << "0";
+        }
+    }
+    cout << nline;
+}
+
+int main()
+{
+    fastio();
+    const ll MAX_N = 10000000;
+    sieve(MAX_N);
+    int tt;
+    cin >> tt;
+    while(tt--)
+    {
+        solve();
+    }
+    return 0;
+}
