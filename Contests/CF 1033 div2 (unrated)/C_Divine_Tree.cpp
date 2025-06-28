57ca43efe376c01ba34571bb892bbb90b038b8ba
rohit <rohitbhagatkar940@gmail.com>
2025-06-28 22:28:31 +0100

    upd

---
+++ Contests/CF 1033 div2 (unrated)/C_Divine_Tree.cpp
@@ -0,0 +1,151 @@
+/*==================================================
+  Author    : ASSaASSin
+  Created   : 28-June-2025
+  Purpose   : Competitive Programming Template
+==================================================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define Assassin
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+const ll MOD = 1e9 + 7;
+const ll INF = LONG_LONG_MAX;
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef unordered_map<ll, ll> umll;
+typedef map<ll, ll> mll;
+
+#define pb push_back
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES" << nline
+#define no cout << "NO" << nline
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define rev(i, a, b) for(int i = (a); i >= (b); i--)
+
+#ifdef Assassin
+#define debug(x) cerr << #x << " " << x << endl;
+#else
+#define debug(x) ;
+#endif
+
+ll gcd(ll x, ll y)
+{
+    if(y == 0)
+        return x;
+    return gcd(y, x % y);
+}
+ll lcm(int x, int y) { return x * y / gcd(x, y); }
+bool isPrime(ll n)
+{
+    if(n <= 1)
+        return false;
+    if(n <= 3)
+        return true;
+    if(n % 2 == 0 || n % 3 == 0)
+        return false;
+    for(int i = 5; i * i <= n; i += 6)
+    {
+        if(n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+bool isPerfectSquare(ll x)
+{
+    if(x >= 0)
+    {
+        ll sr = sqrt(x);
+        return (sr * sr == x);
+    }
+    return false;
+}
+vll primes;
+void sieve(ll MAX_N)
+{
+    vector<bool> prime(MAX_N + 1, true);
+    prime[0] = prime[1] = false;
+    for(ll p = 2; p * p <= MAX_N; p++)
+    {
+        if(prime[p])
+        {
+            for(ll i = p * p; i <= MAX_N; i += p)
+                prime[i] = false;
+        }
+    }
+    for(ll i = 2; i <= MAX_N; i++)
+    {
+        if(prime[i])
+            primes.pb(i);
+    }
+}
+
+void solve()
+{
+    int n; 
+    ll m;
+    cin >> n >> m;
+    // minimum m can be 10
+    if(m < n || m > (1ll*n *(n + 1)) / 2)
+    {
+        cout << -1 << nline;
+        return;
+    }
+    ll rem = 1LL*n *(n+1) / 2 - m;
+    vi v(n);
+    for(int i = 0; i < n; i++)
+    {
+        v[i] = i + 1;
+    }
+    for(int i = n - 1; i >= 0; i--)
+    {
+        if(v[i] - 1 >= rem)
+        {
+            v[i] = v[i] - rem;
+            rem = 0;
+        }else
+        {
+            rem = rem - (v[i] - 1);
+            v[i] = 1;
+        }
+        if(rem <= 0) break;
+    }
+    int root = *max_element(v.begin(), v.end());
+    cout << root << nline;
+    for(int i = 0; i < n; i++)
+    {
+        if(v[i] == i + 1)
+        {
+            if(i + 1 == root) continue;
+            cout << i + 1 << ' ' << root << nline;
+        }else
+        {
+            cout << i + 1 << ' ' << v[i] << nline;
+        }
+    }
+}
+
+int main()
+{
+    fastio();
+    const ll MAX_N = 10000000;
+    sieve(MAX_N);
+    int tt;
+    cin >> tt;
+    while(tt--)
+    {
+        solve();
+    }
+    return 0;
+}
