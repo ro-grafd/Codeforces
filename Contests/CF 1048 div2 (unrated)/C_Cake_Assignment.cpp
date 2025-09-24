cb0f0df6d7c534f5db31c148f3202a75621edb04
rohit <rohitbhagatkar940@gmail.com>
2025-09-24 22:26:42 +0100

    Day 3

--- Contests/CF 1048 div2 (unrated)/C_Cake_Assignment.cpp
+++ Contests/CF 1048 div2 (unrated)/C_Cake_Assignment.cpp
@@ -1,122 +1,145 @@
-/*==================================================
-  Author    : ASSaASSin
-  Created   : 23-Sept-2025
-  Purpose   : Competitive Programming Template
-==================================================*/
+    /*==================================================
+    Author    : ASSaASSin
+    Created   : 23-Sept-2025
+    Purpose   : Competitive Programming Template
+    ==================================================*/
 
-#include <bits/stdc++.h>
-using namespace std;
-#define Assassin
-#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define Assassin
+    #define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-using ll = long long;
-using lld = long double;
-using ull = unsigned long long;
+    using ll = long long;
+    using lld = long double;
+    using ull = unsigned long long;
 
-const ll mod = 1e9 + 7;
-const ll inf = LONG_LONG_MAX;
+    const ll mod = 1e9 + 7;
+    const ll inf = LONG_LONG_MAX;
 
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-typedef vector<string> vs;
-typedef unordered_map<ll, ll> umpll;
-typedef map<int, int> mpii;
-typedef map<ll, ll> mpll;
+    typedef vector<int> vi;
+    typedef vector<ll> vll;
+    typedef pair<int, int> pii;
+    typedef pair<ll, ll> pll;
+    typedef vector<pii> vpii;
+    typedef vector<pll> vpll;
+    typedef vector<string> vs;
+    typedef unordered_map<ll, ll> umpll;
+    typedef map<int, int> mpii;
+    typedef map<ll, ll> mpll;
 
-#define pb push_back
-#define mp make_pair
-#define nline '\n'
-#define yes cout << "YES" << nline
-#define no cout << "NO" << nline
-#define rep(i, a, b) for(int i = (a); i < (b); i++)
-#define rev(i, a, b) for(int i = (a); i >= (b); i--)
+    #define pb push_back
+    #define mp make_pair
+    #define nline '\n'
+    #define yes cout << "YES" << nline
+    #define no cout << "NO" << nline
+    #define rep(i, a, b) for(int i = (a); i < (b); i++)
+    #define rev(i, a, b) for(int i = (a); i >= (b); i--)
 
-#ifdef Assassin
-#define debug(x) cerr << #x << " " << x << endl;
-#define debugv(v) cerr << #v << ": "; for(auto x : v) cerr << x << " "; cerr << endl;
-#else
-#define debug(x) ;
-#define debugv(v) ;
-#endif
+    #ifdef Assassin
+    #define debug(x) cerr << #x << " " << x << endl;
+    #define debugv(v) cerr << #v << ": "; for(auto x : v) cerr << x << " "; cerr << endl;
+    #else
+    #define debug(x) ;
+    #define debugv(v) ;
+    #endif
 
-// Templates
-template<class T>
-ostream& operator<<(ostream& os, vector<T> v) {
-for(auto x : v) os << x << " ";
-return os;
-}
+    // Templates
+    template<class T>
+    ostream& operator<<(ostream& os, vector<T> v) {
+    for(auto x : v) os << x << " ";
+    return os;
+    }
 
-ll gcd(ll x, ll y)
-{
-    if(y == 0)
-        return x;
-    return gcd(y, x % y);
-}
-ll lcm(int x, int y) { return x * y / gcd(x, y); }
-bool isPrime(ll n)
-{
-    if(n <= 1)
-        return false;
-    if(n <= 3)
-        return true;
-    if(n % 2 == 0 || n % 3 == 0)
-        return false;
-    for(int i = 5; i * i <= n; i += 6)
+    ll gcd(ll x, ll y)
     {
-        if(n % i == 0 || n % (i + 2) == 0)
+        if(y == 0)
+            return x;
+        return gcd(y, x % y);
+    }
+    ll lcm(int x, int y) { return x * y / gcd(x, y); }
+    bool isPrime(ll n)
+    {
+        if(n <= 1)
             return false;
-    }
-    return true;
-}
-bool isPerfectSquare(ll x)
-{
-    if(x >= 0)
-    {
-        ll sr = sqrt(x);
-        return (sr * sr == x);
-    }
-    return false;
-}
-vll primes;
-void sieve(ll MAX_N)
-{
-    vector<bool> prime(MAX_N + 1, true);
-    prime[0] = prime[1] = false;
-    for(ll p = 2; p * p <= MAX_N; p++)
-    {
-        if(prime[p])
+        if(n <= 3)
+            return true;
+        if(n % 2 == 0 || n % 3 == 0)
+            return false;
+        for(int i = 5; i * i <= n; i += 6)
         {
-            for(ll i = p * p; i <= MAX_N; i += p)
-                prime[i] = false;
+            if(n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+        return true;
+    }
+    bool isPerfectSquare(ll x)
+    {
+        if(x >= 0)
+        {
+            ll sr = sqrt(x);
+            return (sr * sr == x);
+        }
+        return false;
+    }
+    vll primes;
+    void sieve(ll MAX_N)
+    {
+        vector<bool> prime(MAX_N + 1, true);
+        prime[0] = prime[1] = false;
+        for(ll p = 2; p * p <= MAX_N; p++)
+        {
+            if(prime[p])
+            {
+                for(ll i = p * p; i <= MAX_N; i += p)
+                    prime[i] = false;
+            }
+        }
+        for(ll i = 2; i <= MAX_N; i++)
+        {
+            if(prime[i])
+                primes.pb(i);
         }
     }
-    for(ll i = 2; i <= MAX_N; i++)
-    {
-        if(prime[i])
-            primes.pb(i);
-    }
-}
 
-void solve()
-{
-    ll k, x; cin >> k >> x;
-    
-}
-
-signed main()
-{
-    fastio();
-    const ll MAX_N = 10000000;
-    sieve(MAX_N);
-    int tt;
-    cin >> tt;
-    while(tt--)
+    void solve()
     {
-        solve();
+        ll k, x; cin >> k >> x;
+        ll c = x;
+        ll v = (1LL << (k + 1))  - c;
+        vector<int> a;
+        while(c != (1LL << k))
+        {
+            if(c < v)
+            {
+                v -= c;
+                c *= 2;
+                a.pb(1);
+            }else if(c > v)
+            {
+                c -= v;
+                v *= 2;
+                a.pb(2);
+            }
+        }
+        cout << a.size() << nline;
+        reverse(a.begin(), a.end());
+        for(auto i : a)
+        {
+            cout << i << ' ';
+        }
+        cout << nline;
     }
-    return 0;
-}
+
+    signed main()
+    {
+        fastio();
+        const ll MAX_N = 10000000;
+        sieve(MAX_N);
+        int tt;
+        cin >> tt;
+        while(tt--)
+        {
+            solve();
+        }
+        return 0;
+    }
